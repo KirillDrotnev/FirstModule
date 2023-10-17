0a29a4313a338db06ed72f0bbe65ac1b05f1e643
Evgeni <agenskievgeni@gmail.com>
2023-10-17 23:27:30 +0300

    input method creation

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void Main()
+{
+
+}
+
+(string[]) Input()
+{
+    Console.WriteLine("Введите количество элементов в массиве: ");
+    int count = int.Parse(Console.ReadLine());
+    string[] array = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"Введите элемент №{i + 1}");
+        count[i] = Console.ReadLine();
+    } 
+}
