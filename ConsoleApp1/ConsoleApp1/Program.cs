0d28bc00f29bd89bb7afbeae39fb5212f55689a0
Evgeni <agenskievgeni@gmail.com>
2023-10-17 23:34:41 +0300

    sample creation

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,7 +3,8 @@
 
 }
 
-(string[]) Input()
+// Получениие данных от пользователя
+string[] Input()
 {
     Console.WriteLine("Введите количество элементов в массиве: ");
     int count = int.Parse(Console.ReadLine());
@@ -13,4 +14,20 @@
         Console.WriteLine($"Введите элемент №{i + 1}");
         count[i] = Console.ReadLine();
     } 
+}
+
+string[] Sampling(string[] input)
+{
+    string[] result = new string[input.Length];
+    for (int i = 0;i < input.Length;i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            for (int j = 0;j < result.Length; j++)
+            {
+                result[j] = input[i];
+            }
+        }
+    }
+    return result;
 }
